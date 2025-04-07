2f5ec295e1c3efeb5bc61574aaad350987312569
maxie <maxie98@proton.me>
2025-04-07 05:45:26 -0500

    notes on changing product type

--- Package.swift
+++ Package.swift
@@ -7,10 +7,13 @@ let package = Package(
     name: "swift-sucky",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-t         // ok maybe i need to change this 
-        .library(
+        // ok maybe i need to change this
+        // trying to change it, confirmed '.executable' with same name/targets params
+        // and it just inserted the whole snippet for a new .executable? what happened
+        // to smart replacing? pointless to manually delete the rest of the template :(
+        .executable(name: String, targets: [String])(
             name: "swift-sucky",
-            targets: ["swift-sucky"]),
+            targets: ["swift-sucky"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
