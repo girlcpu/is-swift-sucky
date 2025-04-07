8ef50ceb85726c29dc3902fa974b8bf52970946f
maxie <maxie98@proton.me>
2025-04-07 05:55:23 -0500

    read input

--- Sources/swift-sucky/main.swift
+++ Sources/swift-sucky/main.swift
@@ -11,9 +11,9 @@
 // changed it to executable and still no top level? i really thought we had that
 // ok so then what do i need to do? main fn?
 
-public func main() {
-    print("Hello!")
-}
+// public func main() {
+//     print("Hello!")
+// }
 
 // no errors with mew public func main()
 // lets try to run this
@@ -21,4 +21,26 @@ public func main() {
 // i can do that
 // so i should be able to call that top level
 
-main()
+// main()
+
+// IT PRINTED HELLO, NICE
+
+// what else should I make it do? read from stdin?
+// good old calculator
+
+print("Calculator")
+
+// i think print() does println() newline
+// is there a regular print or prompt builtin?
+
+// import Foundation
+
+// nothing useful coming in from Foundation
+// time to start searching the web
+
+// ok nice we have readLine()
+// probably don't even need Foundation, commenting
+
+if let expression = readLine() {
+    print("TEST: You submitted the expression: \(expression)")
+}
