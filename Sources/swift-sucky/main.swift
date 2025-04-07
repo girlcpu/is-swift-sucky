5de77e858abb98e5929b299fc223f0e8bb556fc2
maxie <maxie98@proton.me>
2025-04-07 05:48:44 -0500

    pawgress update

--- Sources/swift-sucky/main.swift
+++ Sources/swift-sucky/main.swift
@@ -4,8 +4,21 @@
 // print("I didn't get any IntelliSense help here");
 // oh i forgot the extension pack
 // ok i had to wait like 10 seconds for the first suggestion but we good
-print("Hello, Chungusville!")
+// print("Hello, Chungusville!")
 // expressions are not allowed at top level
 // i thought you can write scripts in swift too?
 // maybe its a target issue in package.swift
 // changed it to executable and still no top level? i really thought we had that
+// ok so then what do i need to do? main fn?
+
+public func main() {
+    print("Hello!")
+}
+
+// no errors with mew public func main()
+// lets try to run this
+// oh apparently an executable target requires a main.swift file
+// i can do that
+// so i should be able to call that top level
+
+main()
